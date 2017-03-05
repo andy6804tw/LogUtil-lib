540dbd38b3819671aeaff67c9c16e2678d036d4e
andy6804tw <andy6804tw@yahoo.com.tw>
2017-03-05 21:25:26 +0800

    First Commit Android Library for working with Log file in Android

---
+++ logutil/src/main/java/com/openweather/logutil/LogDebug.java
@@ -0,0 +1,14 @@
+package com.openweather.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by andy6804tw on 2017/3/5.
+ */
+
+public class LogDebug {
+    private static final  String TAG="LogUtil Library:";
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+}
